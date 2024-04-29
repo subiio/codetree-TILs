b51be3d1293562ac74a6161c48e3e059204657a2
subiio <156166363+subiio@users.noreply.github.com>
2024-04-29 21:29:49 +0900

    Title: 배열 채우고 출력하기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240429/배열 채우고 출력하기/filling-array-and-print.py
+++ 240429/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,12 +1,12 @@
-# a = input().split()
-# i = len(a)
-# while i != 0:
-#     i -= 1
-#     print(a[i],end='')
+a = input().split()
+i = len(a)
+while i != 0:
+    i -= 1
+    print(a[i],end='')
 
 
-a = list(map(int,input().split()))
+# a = list(map(int, input().split()))
 
-i=len(a)
-for j in range(len(a)-1,-1,-1):
-    print(a[j],end='')
+# i=len(a)
+# for j in range(i-1,-1,-1):
+#     print(a[j],end='')
