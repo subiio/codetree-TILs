9505aa5d96daabe4161d8e5fc88ee8415e530a9c
subiio <156166363+subiio@users.noreply.github.com>
2024-04-29 21:27:11 +0900

    Title: 배열 채우고 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240429/배열 채우고 출력하기/filling-array-and-print.py
+++ 240429/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,5 +1,12 @@
-a = input().split()
-i = len(a)
-while i != 0:
-    i -= 1
+# a = input().split()
+# i = len(a)
+# while i != 0:
+#     i -= 1
+#     print(a[i],end='')
+
+
+a = list(map(int,input().split()))
+
+i=len(a)
+for i in range(len(a)-1,-1,-1):
     print(a[i],end='')
