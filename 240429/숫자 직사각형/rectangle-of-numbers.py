1a0121ee70f4db5d9f74d7e7eb20a745146e1f02
subiio <156166363+subiio@users.noreply.github.com>
2024-04-29 22:13:05 +0900

    Title: 숫자 직사각형, Time: 181ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240429/숫자 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,16 @@
+n,m = map(int,input().split())
+
+arr_2d =[ [0 for i in range(m)]
+    for j in range(n)]
+
+cnt = 1
+
+for i in range(n):
+    for j in range(m):
+        arr_2d[i][j] = cnt
+        cnt += 1
+
+for row in arr_2d:
+    for elem in row:
+        print(elem, end= " ")
+    print()
