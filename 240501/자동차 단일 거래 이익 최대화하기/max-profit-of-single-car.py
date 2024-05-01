2347b12e8eaa00fbd54947b17a440d4bc1ecbcd5
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 12:19:11 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240501/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -3,8 +3,14 @@ years = int(input())
 price_list = list(map(int,input().split()))
 
 difference_list = []
-for i in range(years):
-    for j in range(i+1,years):
-        difference_list.append(price_list[j]-price_list[i])
+if years == 1:
+    print(0)
+else:
+    for i in range(years):
+        for j in range(i+1,years):
+            difference_list.append(price_list[j]-price_list[i])
 
-print(max(difference_list))
+if max(difference_list) > 0:
+    print(max(difference_list))
+else:
+    print(0)
