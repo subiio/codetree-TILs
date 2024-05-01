379b7734c33fc8af39903785451abf2aa0db01f3
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 13:32:16 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240501/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,14 @@
+n,m = map(int,input().split())
+list1 = []
+list2 = []
+for _ in range(m):
+    a = list(map(int,input().split()))
+    list1.append(a)
+for _ in range(m):
+    a = list(map(int,input().split()))
+    list2.append(a)
+
+for i in range(n):
+    for j in range(m):
+        print( 0 if list1[i][j] == list2[i][j] else 1, end = ' ' )
+    print()
