aa9f697aadd34d912651d1445293d1f41e6e4d71
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 10:03:30 +0900

    Title: 짝수인 것만 출력하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/짝수인 것만 출력하기/print-even-number.py
@@ -0,0 +1,13 @@
+n = input()
+n = int(n)
+list2 = []
+list1 = list(map(int,input().split()))
+
+for i in list1:
+    if i%2 == 0:
+        list2.append(i)
+    else:
+        pass
+
+for j in list2:
+    print(j)
