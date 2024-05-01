13f5be8d01f84be66c4062d8fec84e12cf6ef39f
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 19:50:26 +0900

    Title: 2개씩 그룹짓기, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240501/2개씩 그룹짓기/group-of-pairs.py
+++ 240501/2개씩 그룹짓기/group-of-pairs.py
@@ -8,6 +8,6 @@ if N == 1:
     print(sum(list1))
 else:
     for i in range(N):
-        list2.append(list1[i] + list1[N+1-i])
+        list2.append(list1[i] + list1[2*N-1-i])
 
     print(max(list2))
