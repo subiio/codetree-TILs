dabeafb60e5c883a0c73d069548333af8c8450fc
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 12:19:55 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 143ms, Memory: 35MB, Status: Passed - CodeTree

--- 240501/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240501/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -9,8 +9,8 @@ else:
     for i in range(years):
         for j in range(i+1,years):
             difference_list.append(price_list[j]-price_list[i])
-
-if max(difference_list) > 0:
-    print(max(difference_list))
-else:
-    print(0)
+    
+    if max(difference_list) > 0:
+        print(max(difference_list))
+    else:
+        print(0)
