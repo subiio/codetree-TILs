d3b3095ba8863f500a1a2f16a64a03c32fbb22d5
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 19:48:36 +0900

    Title: 2개씩 그룹짓기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/2개씩 그룹짓기/group-of-pairs.py
+++ 240501/2개씩 그룹짓기/group-of-pairs.py
@@ -4,7 +4,10 @@ list1 = list(map(int,input().split()))
 list1 = sorted(list1)
 
 list2 = []
-for i in range(N):
-    list2.append(list1[i] + list1[N+1-i])
+if N == 1:
+    print(sum(list1))
+else:
+    for i in range(N):
+        list2.append(list1[i] + list1[N+1-i])
 
-print(max(list2))
+    print(max(list2))
