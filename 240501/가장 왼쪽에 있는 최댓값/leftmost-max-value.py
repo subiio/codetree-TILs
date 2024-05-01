0ff8622dcc2a062f7d35f3581cf3fe7cdacb262e
subiio <156166363+subiio@users.noreply.github.com>
2024-05-01 12:08:29 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240501/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240501/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,22 +1,18 @@
-N = input()
-N = int(N)
+n = int(input())
+a = list(map(int,input().split()))
 
-list1 = list(map(int,input().split()))
+prev_max_idx = n
 
-max_list = max(list1)
-count = 0
-list2 = []
-for i in list1:
-    count += 1
-    list2.append(i)
-    if max_list == i:
+while True:
+    max_idx =0
+
+    for i in range(1,prev_max_idx):
+        if a[i] > a[max_idx]:
+            max_idx = i
+    
+    print(max_idx + 1, end =" ")
+
+    if max_idx == 0:
         break
-print(count, end= ' ')
-if not count == 1:
-    max_list2 = max(list2)
-    count2 = 0
-    for j in list2:
-        count2 += 1
-        if max_list2 == i:
-            break
-    print(count2, end = ' ')
+    
+    prev_max_idx = max_idx
