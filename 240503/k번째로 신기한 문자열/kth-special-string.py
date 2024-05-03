da72e4cb05f0aa291d9020079c273a8b69cafe6e
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 14:08:42 +0900

    Title: k번째로 신기한 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240503/k번째로 신기한 문자열/kth-special-string.py
+++ 240503/k번째로 신기한 문자열/kth-special-string.py
@@ -2,9 +2,14 @@ n,k,T =input().split()
 n = int(n)
 k = int(k)
 list1 = []
+
 for _ in range(n):
     a = input()
-    if a[0] == 'a' and a[1] == 'p':
+    count = 0
+    for i in range(len(T)):
+        if a[i] == T[i] :
+            count += 1
+    if count == len(T):
         list1.append(a)
 
 print(sorted(list1)[k-1])
