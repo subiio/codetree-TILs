f9b4087e414089c3c968717ebb80ee476393d96e
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 20:37:52 +0900

    Title: 블럭쌓는 명령2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240503/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240503/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,7 +3,7 @@ N,K = map(int,input().split())
 list2 = [0]*N 
 list1 = []
 for _ in range(K):
-    list1.append(tuple(map(int,input().split())))
+    list1.append(list(map(int,input().split())))
 
 for i in range(len(list1)):
     for j in range(list1[i][0],list1[i][1]+1):
