02ee77e7572667494a91eab45401104cccbfcc89
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 16:28:52 +0900

    Title: 키를 기준으로 정렬, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240503/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+class Person:
+    def __init__(self,name,height,weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+people= []
+for _ in range(n):
+    name,height,weight = tuple(input().split())
+    people.append(Person(name,height,weight))
+
+
+
+people.sort(key= lambda x: x.height)
+
+for i in range(n):
+    print(people[i].name,people[i].height,people[i].weight)
