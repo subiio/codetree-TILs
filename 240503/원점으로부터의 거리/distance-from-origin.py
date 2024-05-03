5b4c1eefa35bdef77de31c1bc97f748c4564e5ae
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 17:12:14 +0900

    Title: 원점으로부터의 거리, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,20 @@
+N =int(input())
+
+
+class Point:
+    def __init__(self,x,y,number):
+        self.x = x
+        self.y = y
+        self.number= number
+
+point = [
+]
+
+for i in range(1,N+1):
+    x,y = tuple(input().split())
+    point.append(Point(x,y,i))
+
+point.sort(key = lambda x: ((int(x.x))^2 + (int(x.y))^2))
+
+for i in range(N):
+    print(point[i].number)
