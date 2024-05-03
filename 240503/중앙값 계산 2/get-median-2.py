9c6b016f0c5b8b915d09e3816dddbb7e0308014c
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 14:49:20 +0900

    Title: 중앙값 계산 2, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/중앙값 계산 2/get-median-2.py
+++ 240503/중앙값 계산 2/get-median-2.py
@@ -6,7 +6,7 @@ list1 = list(map(int,input().split()))
 def find_center(num_list):
     a = sorted(num_list)
     lens = len(a)
-    return num_list[lens//2]
+    return a[lens//2]
 list2 = []
 for i in range(len(list1)):
     list2.append(list1[i])
