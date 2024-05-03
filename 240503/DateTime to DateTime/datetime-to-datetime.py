7fc97aa856bdb64b95f3c535fb3811b424bb1ffc
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 19:32:37 +0900

    Title: DateTime to DateTime, Time: 5042ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240503/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,18 @@
+a,b,c = map(int,input().split())
+start_a = 11
+start_b = 11
+start_c = 11
+time_total = 0
+while True:
+    if start_a == a and start_b == b and start_c == c:
+        break
+    time_total += 1
+    start_c += 1
+    if start_c == 60:
+        start_c = 0
+        start_b += 1
+    if start_b == 24:
+        start_b = 0
+        start_a += 1
+
+print(time_total)
