eb03c9b09a64d6a918cdae7b9582d04555576ed7
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 16:56:23 +0900

    Title: 줄 세우기, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/줄 세우기/line-up-students.py
+++ 240503/줄 세우기/line-up-students.py
@@ -12,7 +12,7 @@ for i in range(1,N+1):
     height,weight = tuple(input().split())
     people.append(body(height,weight,i))
 
-people.sort(key = lambda x: (-int(x.height),-int(x.weight),-int(x.number)))
+people.sort(key = lambda x: (-int(x.height),-int(x.weight),int(x.number)))
 
 for j in range(N):
     print(people[j].height,people[j].weight,people[j].number)
