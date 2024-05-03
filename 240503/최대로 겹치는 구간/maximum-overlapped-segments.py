471d20547cf3c4bf08a86757c7a0906bb4fea65c
subiio <156166363+subiio@users.noreply.github.com>
2024-05-03 20:55:42 +0900

    Title: 최대로 겹치는 구간, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240503/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-list1 = [0] *400
+list1 = [0] *(200+1)
 list2 = []
 for i in range(n):
     list2.append(list(map(int,input().split())))
 
 for i in range(len(list2)):
-    for j in range(list2[i][0],list2[i][1]-1):
+    for j in range(list2[i][0]+100,list2[i][1]+100):
         list1[j] += 1
 
 print(max(list1))
