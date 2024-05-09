a87e34d2608310a0ceac23df3ffc0f85856da1b8
subiio <156166363+subiio@users.noreply.github.com>
2024-05-09 20:04:05 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,8 @@
+brackets = input()
+S = 0
+for i in range(len(brackets)):
+    if brackets[i] == ')':
+        continue
+    for j in range(i+1,len(brackets)):
+        if brackets[j] == ')':
+            S += 1
