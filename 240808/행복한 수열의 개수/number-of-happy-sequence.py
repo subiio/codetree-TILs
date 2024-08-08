3664a6572d435df5947b41a686ce1ee040334633
subiio <156166363+subiio@users.noreply.github.com>
2024-08-08 10:58:12 +0900

    Title: 행복한 수열의 개수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,49 @@
+n, m = map(int,input().split())
+suyeol = []
+for _ in range(n):
+    suyeol.append(list(map(int,input().split())))
+
+
+def count_numbers(a):
+    list = []
+    for i in range(1,10):
+        list.append(a.count(i))
+    list2 = []
+    for j in range(len(list)):
+        if list[j] >= m:
+            list2.append(j+1)
+    count = 0
+    for i in list2:
+        for j in a:
+            if j != i:
+                count = 0
+            else:
+                count += 1
+    if count >= 2:
+        return True
+
+
+    return False
+
+for i in range(n):
+    list = []
+    for j in range(n):
+        list.append(suyeol[j][i])
+
+        if j == 2:
+            suyeol.append(list)
+
+count = 0
+for i in suyeol:
+    hi = count_numbers(i)
+    if hi == True:
+        count += 1
+print(count)
+
+
+
+# #######
+# [0][0],[1][0],[2][0]
+# [0][1],[1][1],[2][1]
+# [0][2],
+# #######
