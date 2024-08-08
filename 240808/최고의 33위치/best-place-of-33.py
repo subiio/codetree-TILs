f7b5877901b3c9a5c899d3123dda259aaa0bce68
subiio <156166363+subiio@users.noreply.github.com>
2024-08-08 10:25:08 +0900

    Title: 최고의 33위치, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/최고의 33위치/best-place-of-33.py
+++ 240808/최고의 33위치/best-place-of-33.py
@@ -5,11 +5,11 @@ for _ in range(N):
     coins.append(list(map(int,input().split())))
 
 max_coin = 0
-tmp = 0
 for i in range(N):
     for j in range(N):
         if i+2 >= N or j+2 >= N:
             continue
+        tmp = 0
         for k in range(i, i+3):
             for l in range(j, j+3):
                 if coins[k][l] == 1:
