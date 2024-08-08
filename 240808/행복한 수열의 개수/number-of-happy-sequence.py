c456b1145bd4c99b61e6c94b79f8e368a32f3fb2
subiio <156166363+subiio@users.noreply.github.com>
2024-08-08 11:21:26 +0900

    Title: 행복한 수열의 개수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240808/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240808/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,52 +1,33 @@
-n, m = map(int,input().split())
+n, m = map(int, input().split())
 suyeol = []
 for _ in range(n):
-    suyeol.append(list(map(int,input().split())))
+    suyeol.append(list(map(int, input().split())))
 
 
-def count_numbers(a):
-    list = []
-    for i in range(1,101):
-        list.append(a.count(i))
-    list2 = []
-    for j in range(len(list)):
-        if list[j] >= m:
-            list2.append(j+1)
-    count1 = 0
-    for k in list2:
-        for l in a:
-            if l != k:
-                count1 = 0
-            else:
-                count1 += 1
-                if count1 >= m:
-                    return True
+def count_numbers(a, m):
+    count_dict = {}
+    for num in a:
+        if num in count_dict:
+            count_dict[num] += 1
+        else:
+            count_dict[num] = 1
 
-
+    for num, count in count_dict.items():
+        if count >= m:
+            return True
     return False
 
-for i in range(n):
-    list = []
-    for j in range(n):
-        list.append(suyeol[j][i])
 
-        if j == n-1:
-            suyeol.append(list)
+# 열 추가하기
+for i in range(n):
+    col = []
+    for j in range(n):
+        col.append(suyeol[j][i])
+    suyeol.append(col)
 
 count = 0
-for i in suyeol:
-    hi = count_numbers(i)
-    if hi == True:
+for row in suyeol:
+    if count_numbers(row, m):
         count += 1
-if len(suyeol) == 1:
-    print(2)
-else:
-    print(count)
 
-
-
-# #######
-# [0][0],[1][0],[2][0]
-# [0][1],[1][1],[2][1]
-# [0][2],
-# #######
+print(count)
