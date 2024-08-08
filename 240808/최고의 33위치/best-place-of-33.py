eaf2e9f9a596be4e4f53df80360577197145cb21
subiio <156166363+subiio@users.noreply.github.com>
2024-08-08 10:24:18 +0900

    Title: 최고의 33위치, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,20 @@
+N = int(input())
+coins = []
+
+for _ in range(N):
+    coins.append(list(map(int,input().split())))
+
+max_coin = 0
+tmp = 0
+for i in range(N):
+    for j in range(N):
+        if i+2 >= N or j+2 >= N:
+            continue
+        for k in range(i, i+3):
+            for l in range(j, j+3):
+                if coins[k][l] == 1:
+                    tmp += 1
+        if tmp >= max_coin:
+            max_coin = tmp
+
+print(max_coin)
