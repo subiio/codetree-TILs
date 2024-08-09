f371ef32a8e3c30e388725af5de7b91553fcab44
subiio <156166363+subiio@users.noreply.github.com>
2024-08-09 11:25:08 +0900

    Title: 트로미노, Time: 150ms, Memory: 28MB, Status: Passed - CodeTree

--- 240809/트로미노/tromino.py
+++ 240809/트로미노/tromino.py
@@ -4,6 +4,9 @@ grid = []
 for _ in range(n):
     grid.append(list(map(int,input().split())))
 
+# grid = [[1, 1, 1, 4],
+# [1, 1, 1, 4],
+# [1, 1, 1, 4]]
 
 
 # key1 [[i, j],[i, j+1], [i+1, j]]
@@ -23,8 +26,9 @@ key = [ [[0, 0],[0, 1], [1, 0]],
 
 def find_max(key,grid):
     # example of key : [[0,0], [0,1], [1,0]]
-    n = len(grid[0])
-    m = len(grid)
+    m = len(grid[0])
+    n = len(grid)
+    # print(n,m)
     first = key[0]
     second = key[1]
     third = key[2]
@@ -32,13 +36,14 @@ def find_max(key,grid):
     tmp = 0
     for i in range(n):
         for j in range(m):
-            if ((i+first[0]) < m and (j+first[1]) < n) and ((i+second[0]) < m and (j+second[1] < n) and ((i+third[0])< m and (j+third[1]) < n)):
+            if ((i+first[0]) < n and (j+first[1]) < m) and ((i+second[0]) < n and (j+second[1] < m) and ((i+third[0])< n and (j+third[1]) < m)):
                 tmp = (grid[i+first[0]][j+first[1]] + grid[i+second[0]][j+second[1]] + grid[i+third[0]][j+third[1]])
             maximum = max(maximum, tmp)
     return maximum
 
 maximum_list = []
 for k in range(len(key)):
+    # print(key[k])
     maximum_list.append(find_max(key[k], grid))
 
 print(max(maximum_list))
