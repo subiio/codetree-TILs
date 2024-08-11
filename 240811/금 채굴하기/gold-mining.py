10b8b7f154f324e4f298347f00e6613d8c88c17b
subiio <156166363+subiio@users.noreply.github.com>
2024-08-11 10:06:01 +0900

    Title: 금 채굴하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240811/금 채굴하기/gold-mining.py
+++ 240811/금 채굴하기/gold-mining.py
@@ -1,69 +1,36 @@
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 
 grid = []
-
 for _ in range(n):
-    grid.append(list(map(int,input().split())))
+    grid.append(list(map(int, input().split())))
 
+# 누적 합 배열 계산
+prefix_sum = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix_sum[i][j] = (grid[i - 1][j - 1] + 
+                            prefix_sum[i - 1][j] + 
+                            prefix_sum[i][j - 1] - 
+                            prefix_sum[i - 1][j - 1])
 
-#######################
-#        [1, 1]
-# [2, 0] [2, 1] [2, 2]
-#        [3, 1] 
-#######################
-# 0 <= K <= 19
-#
-# i = 0 , 
-#
-#
-#
-#######################
+def get_sum(x1, y1, x2, y2):
+    return (prefix_sum[x2 + 1][y2 + 1] - 
+            prefix_sum[x1][y2 + 1] - 
+            prefix_sum[x2 + 1][y1] + 
+            prefix_sum[x1][y1])
 
-
-def find_key(K,center):
-    key_list = [center]
-    key = [[0,1],[0,-1],[1,0],[-1,0]] 
-    if K == 0:
-        return key_list
-    for i in range(1, K+1):
-        for j in range(len(key_list)):
-            number1 = [key_list[j][0] + key[0][0], key_list[j][1] + key[0][1]]
-            number2 = [key_list[j][0] + key[1][0], key_list[j][1] + key[1][1]]
-            number3 = [key_list[j][0] + key[2][0], key_list[j][1] + key[2][1]]
-            number4 = [key_list[j][0] + key[3][0], key_list[j][1] + key[3][1]]
-            if not (number1 in key_list):
-                key_list.append(number1)
-            if not (number2 in key_list):
-                key_list.append(number2)
-            if not (number3 in key_list):
-                key_list.append(number3)
-            if not (number4 in key_list):
-                key_list.append(number4)
-
-    return key_list
-
-K = len(grid)
 max_gold = 0
-for i in range(K):
-    cost = i*i + (i+1)*(i+1)
-    for j in range(n):
-        for k in range(n):
-            tmp = 0
-            key_list = find_key(i, [j,k])
-            for l in key_list:
-                if (0 <= l[0] < n) and (0 <= l[1] < n) :
-                    if grid[l[0]][l[1]] == 1:
-                        tmp += 1
-            if tmp*m >=  cost and tmp > max_gold:
+
+for k in range(n):
+    cost = k * k + (k + 1) * (k + 1)
+    for i in range(n):
+        for j in range(n):
+            left = max(0, j - k)
+            right = min(n - 1, j + k)
+            up = max(0, i - k)
+            down = min(n - 1, i + k)
+            tmp = get_sum(up, left, down, right)
+            if tmp * m >= cost and tmp > max_gold:
                 max_gold = tmp
 
 print(max_gold)
-
-
-
-
-        
-    
-
-
-# def find_max()
