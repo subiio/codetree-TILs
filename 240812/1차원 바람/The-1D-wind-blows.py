6d6a247b65f6666d26eafb217466cdede9f3edf8
subiio <156166363+subiio@users.noreply.github.com>
2024-08-12 18:29:42 +0900

    Title: 1차원 바람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240812/1차원 바람/The-1D-wind-blows.py
@@ -0,0 +1,57 @@
+N,M,Q = map(int,input().split())
+grid= []
+for _ in range(N):
+    grid.append(list(map(int,input().split())))
+
+moving = []
+for _ in range(Q):
+    moving.append(list(map(str,input().split())))
+
+def toggle(d):
+    if d == "L":
+        d = "R"
+    else:
+        d = "L"
+    return d
+
+def rotation(grid,r,d,M):
+    if d == "L":
+        temp = grid[r-1][M-1]
+        for i in range(M-1,0,-1):
+            grid[r-1][i] = grid[r-1][i-1]
+        
+        grid[r-1][0] = temp
+    else:
+        temp = grid[r-1][0]
+        for i in range(1,M):
+            grid[r-1][i-1] = grid[r-1][i]
+        grid[r-1][M-1] = temp
+    return grid
+
+for i in moving:
+    first_moving_direction = i[1]
+    grid = rotation(grid,int(i[0]),i[1],M)
+    for j in range(int(i[0])-1, 0, -1):
+        i[1] = toggle(i[1])
+        for k in range(M):
+            if (grid[j][k] == grid[j-1][k]):
+                grid = rotation(grid,j,i[1],M)
+
+                break
+            
+    i[1] = first_moving_direction
+    for l in range(int(i[0]), M+1):
+        # print(l)
+        i[1] = toggle(i[1])
+        # print("i[1]:", i[1])
+        for m in range(M):
+            # print("m: ",m)
+            if (grid[l][m] == grid[l-1][m]):
+                grid = rotation(grid,l+1,i[1],M)
+
+                break
+
+for i in range(N):
+    for j in range(M):
+        print(grid[i][j], end = " ")
+    print()
