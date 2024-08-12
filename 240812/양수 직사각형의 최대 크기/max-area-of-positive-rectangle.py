d526e6edcf813657d13ae37db8131d8493c339d5
subiio <156166363+subiio@users.noreply.github.com>
2024-08-12 15:31:19 +0900

    Title: 양수 직사각형의 최대 크기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240812/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240812/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -10,18 +10,18 @@ def find_max(center,grid,n,m):
     for i in range(n):
         for j in range(m):
             tmp = []
-            if i >= 1 and j >=1:
-                for k in range(i+1):
-                    for l in range(j+1):
-                        if 0 <= (center[0] + k) < n and 0<= (center[1] + l) < n:
-                            if grid[center[0] + k][center[1] + l] > 0:
-                                tmp.append([center[0] + k,center[1] + l])
-                            else:
-                                tmp.append(-1)
+            # if i >= 1 and j >=1:
+            for k in range(i+1):
+                for l in range(j+1):
+                    if 0 <= (center[0] + k) < n and 0<= (center[1] + l) < n:
+                        if grid[center[0] + k][center[1] + l] > 0:
+                            tmp.append([center[0] + k,center[1] + l])
                         else:
                             tmp.append(-1)
-                if -1 in tmp:
-                    tmp = [-1]        
+                    else:
+                        tmp.append(-1)
+            if -1 in tmp:
+                tmp = [-1]        
             searchable_list.append(tmp)
     # print("searchable_list:", searchable_list)
     return searchable_list
@@ -40,7 +40,7 @@ for i in center:
         # print(len(j))
         tmp = len(j)
         max_length = max(max_length,tmp)
-if 0 <= max_length < 4:
+if 0 <= max_length < 2:
     print(-1)
 else:
     print(max_length)
