1a4586c88818e76de8fedc3331c5403e8b25ba2c
subiio <156166363+subiio@users.noreply.github.com>
2024-08-23 11:38:02 +0900

    Title: 십자 모양 폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240823/십자 모양 폭발/cross-shape-bomb.py
+++ 240823/십자 모양 폭발/cross-shape-bomb.py
@@ -4,13 +4,14 @@ for _ in range(n):
     grid.append(list(map(int,input().split())))
 
 r,c = map(int,input().split())
-
-# n = grid[r-1][c-1]
-# grid = [[1, 2, 4, 3],
-# [3, 2, 2, 3],
-# [3, 1, 6, 2],
-# [4, 5, 4, 4]]
-# r,c = 3,3
+# n = 6
+# grid =[[1, 1, 1, 1, 1, 1],
+# [1, 2, 4, 3, 1, 1],
+# [3, 1, 2, 3, 1, 1],
+# [3, 1, 6, 2, 1, 1],
+# [4, 5, 4, 1, 1, 1],
+# [1, 1, 1, 1, 1, 1]]
+# r,c = 4,4
 def range_check(grid,r,c):
     right,left,low,high = 0,0,0,0
     
@@ -82,12 +83,12 @@ for p in grid_list:
     a = p[0]
     b = p[1]
     grid[a][b] = 0
-
+aing_list1 = []
+aing_list2 = []
 for y in grid_list:
     d = y[0]
     b = y[1]
-    aing_list1 = []
-    aing_list2 = []
+    # print("b: ",b)
     if b < c-1 or b > c-1:
         # print("b: ", b)
     
@@ -95,26 +96,29 @@ for y in grid_list:
         for i in range(d,0,-1):
             grid[i][b] = grid[i-1][b]
         grid[0][b] = 0
-    else:
+    elif b == c-1:
         aing_list1.append(d)
         aing_list2.append(b)
 
 # print(grid)
 min_range = min(aing_list1)
-max_range = max(aing_list2)
-
+max_range = max(aing_list1)
 range_list = []
-for t in range(0,(min_range)):
+for t in range(0,min_range):
     range_list.append(grid[t][c-1])
 
 for u in range(max_range+1,n):
     range_list.append(grid[u][c-1])
 
 
-for q in range(n-1, n-len(range_list)-1, -1):
-    # print("q: ", q)
-    # print(q-n+len(range_list)-1)
-    grid[q][c-1] = range_list[q-n+len(range_list)-1]
+# for q in range(n-1, n-len(range_list)-1, -1):
+count = 0
+for q in range(n-len(range_list),n):
+    
+    grid[q][c-1] = range_list[count]
+    count += 1
+for v in range(len(range_list)):
+    grid[v][c-1] = 0
 
 # print(grid)
 
