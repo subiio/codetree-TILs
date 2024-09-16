a5a14c2950ce23f932ef15521d74d03326a82c7e
subiio <156166363+subiio@users.noreply.github.com>
2024-09-16 22:04:06 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 410ms, Memory: 30MB, Status: Passed - CodeTree

--- 240916/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 240916/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -15,7 +15,7 @@ def solution(curr_num):
 
     else:
         for i in range(1,K+1):
-            if (curr_num <= 2) or (answer[-1] != i and answer[-2] != i) or (answer[-1] != i):
+            if (curr_num <= 2) or (answer[-1] != i and answer[-2] != i) or (answer[-1] != i) or (answer[-2] != i):
                 answer.append(i)
                 solution(curr_num + 1)
                 answer.pop()
