190511a2b22c95a0d114193fac134153eca6548f
subiio <156166363+subiio@users.noreply.github.com>
2024-09-18 15:13:37 +0900

    Title: 줄 세우기 2, Time: 236ms, Memory: 26MB, Status: Passed - CodeTree

--- 240918/줄 세우기 2/line-up-students-2.py
+++ 240918/줄 세우기 2/line-up-students-2.py
@@ -3,14 +3,14 @@ stu_list = []
 for _ in range(N):
     stu_list.append(list(map(int,input().split())))
 
-# N = 4
-# stu_list = [[1,5],[1,7],[3,6],[1,10]]
 
 
+stu_list2 = stu_list.copy()
 sorted_list = []
+ind_list = []
 
 while N != len(sorted_list):
-    min_ind = -1
+    min_ind = 0
     for i in range(len(stu_list)):
         if stu_list[i][0] < stu_list[min_ind][0]:
             min_ind = i
@@ -21,9 +21,13 @@ while N != len(sorted_list):
                 pass
         else:
             pass
+    for j in range(len(stu_list2)):
+        if stu_list2[j] == stu_list[min_ind]:
+            ind_list.append(j+1)        
+
     sorted_list.append(stu_list[min_ind])
     stu_list.pop(min_ind)
 
 for i in range(1,len(sorted_list)+1):
-    ind = i -1
-    print(sorted_list[ind][0],sorted_list[ind][1], i)
+    ind = i-1
+    print(sorted_list[ind][0],sorted_list[ind][1], ind_list[ind])
