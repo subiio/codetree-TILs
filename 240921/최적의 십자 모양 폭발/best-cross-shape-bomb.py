9cfd43f8a528cfd704f0addfba8ff1f0a48aa207
subiio <156166363+subiio@users.noreply.github.com>
2024-09-21 15:15:34 +0900

    Title: 최적의 십자 모양 폭발, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240921/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -0,0 +1,89 @@
+n = int(input())
+# [0] 남 [1] 동 [2] 서 [3] 북
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+grid = []
+
+for i in range(n):
+    grid.append(list(map(int,input().split())))
+
+
+def in_range(x,y):
+    return (0<= x < n and 0 <= y < n)
+
+def explode(grid,i,j):
+    # 폭발의 크기
+    n = grid[i][j]
+    explode_grid = [[i,j]]
+    if n == 1:
+        pass
+    else:
+        for k in range(1,n):
+            for l in range(len(dx)):
+                explode_grid.append([i + k*dx[l],j + k*dy[l]])
+    # print(explode_grid)
+    for o in explode_grid:
+        if in_range(o[0],o[1]):
+            grid[o[0]][o[1]] = 0
+        else:
+            pass
+    # print(grid)
+    return grid
+
+def gravity(grid,i,j):
+    global n
+    N = i
+    if i == 0:
+        return grid
+    else:
+        for k in range(n):
+            N = i
+            # print(k,N)
+            if grid[N][k] == 0:
+                while N !=0:
+                    # print("N: ", N-1)
+                    # print("grid[N][j]", grid[N][k])
+                    # print("grid[N-1][j]", grid[N-1][k])
+                    grid[N][k] = grid[N-1][k]
+                    N -= 1
+                grid[N][k] = 0
+        # print("gravited:", grid)
+        return grid
+
+# exploded_grid = explode(grid,2,2)
+# gravitied_grid = gravity(exploded_grid,2,2)
+
+def calculate(grid):
+    count = 0
+    for u in range(n):
+        for y in range(n):
+            if grid[u][y] >= 1:
+                for b in range(len(dx)):
+                    dxs = u
+                    dys = y
+                    dxs += dx[b]
+                    dys += dy[b]
+                    if in_range(dxs,dys):
+                        if grid[u][y] == grid[dxs][dys]:
+                            count += 1
+                        else:
+                            pass
+                    else:
+                        pass
+    # print(count/2)
+    return int(count//2)
+
+
+
+
+
+num_list = []
+for i in range(n):
+    for j in range(n):
+        exploded_grid = grid
+        exploded_grid = explode(grid,i,j)
+        gravitied_grid = gravity(exploded_grid,i,j)
+        num_list.append(calculate(gravitied_grid))
+
+print(max(num_list))
