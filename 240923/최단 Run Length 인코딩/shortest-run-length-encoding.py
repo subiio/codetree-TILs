3d976c258cfd25a1de8d351b8bff2b4198fc2fbe
subiio <156166363+subiio@users.noreply.github.com>
2024-09-23 19:58:45 +0900

    Title: 최단 Run Length 인코딩, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240923/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,48 @@
+a = list(input())
+# print(a)
+# TODO
+def calculate_run_length(a):
+    # 초기 length
+    length = 2
+    prev_string = a[0]
+    count = 0
+    for i in a:
+        if prev_string != i:
+            length += 2
+            count += 1
+            prev_string = i
+        else:
+            pass
+    if (count == 0 and len(a) != 10):
+        length = 2
+        return length
+    elif (count == 0 and len(a) == 10):
+        length = 3
+        return length
+    else:
+        return length
+
+
+
+# TODO
+def rotate(a):
+    b = []
+    tmp = a[-1]
+    b.append(tmp)
+    for i in range(0,len(a)-1):
+        b.append(a[i])
+    # print(b)
+    return b
+
+
+
+
+min_length = []
+b = calculate_run_length(a)
+min_length.append(b)
+for _ in range(len(a)-1):
+    a = rotate(a)
+    b = calculate_run_length(a)
+    min_length.append(b)
+
+print(min(min_length))
