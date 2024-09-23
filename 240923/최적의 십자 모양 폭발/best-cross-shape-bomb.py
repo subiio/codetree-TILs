733af3253420182bbe46fec0949c8ddd5f1ee643
subiio <156166363+subiio@users.noreply.github.com>
2024-09-23 21:41:55 +0900

    Title: 최적의 십자 모양 폭발, Time: 2031ms, Memory: 30MB, Status: Passed - CodeTree

--- 240923/최적의 십자 모양 폭발/best-cross-shape-bomb.py
+++ 240923/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -1,86 +1,169 @@
-import copy
+# import copy
 
 
-n = int(input())
-# [0] 남 [1] 동 [2] 서 [3] 북
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+# n = int(input())
+# # [0] 남 [1] 동 [2] 서 [3] 북
+# dx = [1,0,-1,0]
+# dy = [0,1,0,-1]
 
-grid = []
-
-
-for i in range(n):
-    grid.append(list(map(int,input().split())))
-
-# n = 2
-# grid = [[1,1],[1,2]]
+# grid = []
 
 
+# for i in range(n):
+#     grid.append(list(map(int,input().split())))
 
-def in_range(x,y):
-    return (0<= x < n and 0 <= y < n)
+# # n = 2
+# # grid = [[1,1],[1,2]]
 
-def explode(grid_1,i,j):
-    # 폭발의 크기
-    F = grid_1[i][j]
-    explode_grid = [[i,j]]
-    if F == 1:
-        pass
-    else:
-        for k in range(1,F):
-            for l in range(len(dx)):
-                explode_grid.append([i + k*dx[l],j + k*dy[l]])
-    # print(explode_grid)
-    for o in explode_grid:
-        if in_range(o[0],o[1]):
-            grid_1[o[0]][o[1]] = 0
-        else:
-            pass
+
+
+# def in_range(x,y):
+#     return (0<= x < n and 0 <= y < n)
+
+# def explode(grid_1,i,j):
+#     # 폭발의 크기
+#     F = grid_1[i][j]
+#     explode_grid = [[i,j]]
+#     if F == 1:
+#         pass
+#     else:
+#         for k in range(1,F):
+#             for l in range(len(dx)):
+#                 explode_grid.append([i + k*dx[l],j + k*dy[l]])
+#     # print(explode_grid)
+#     for o in explode_grid:
+#         if in_range(o[0],o[1]):
+#             grid_1[o[0]][o[1]] = 0
+#         else:
+#             pass
     
-    return grid_1
+#     return grid_1
 
-def gravity(grid_2,i,j,explode_range):
-    global n
-    N = i
-    if i == 0:
-        return grid_2
-    else:
-        for k in range(n):
-            N = i
-            # print(k,N)
-            if grid_2[N][k] == 0 and k != j:
-                while N !=0:
-                    # print("N: ", N-1)
-                    # print("grid[N][j]", grid[N][k])
-                    # print("grid[N-1][j]", grid[N-1][k])
-                    grid_2[N][k] = grid_2[N-1][k]
-                    N -= 1
-                grid_2[N][k] = 0
-            elif grid_2[N][k] == 0 and k == j:
-                grid_exploded = []
-                try:
-                    # print("N-explode_range + 1: ",)
-                    for e in range(N, N - (explode_range - 1) -1 , -1):
-                        grid_exploded.append(grid_2[e][k])
-                    # print("grid_exploded: ", grid_exploded)
-                    start_ind = 0
-                    for z in range(N+(explode_range - 1), N-1, -1):
+# def gravity(grid_2,i,j,explode_range):
+#     global n
+#     N = i
+#     if i == 0:
+#         return grid_2
+#     else:
+#         for k in range(n):
+#             N = i
+#             # print(k,N)
+#             if grid_2[N][k] == 0 and k != j:
+#                 while N !=0:
+#                     # print("N: ", N-1)
+#                     # print("grid[N][j]", grid[N][k])
+#                     # print("grid[N-1][j]", grid[N-1][k])
+#                     grid_2[N][k] = grid_2[N-1][k]
+#                     N -= 1
+#                 grid_2[N][k] = 0
+#             elif grid_2[N][k] == 0 and k == j:
+#                 grid_exploded = []
+#                 try:
+#                     # print("N-explode_range + 1: ",)
+#                     for e in range(N, N - (explode_range - 1) -1 , -1):
+#                         grid_exploded.append(grid_2[e][k])
+#                     # print("grid_exploded: ", grid_exploded)
+#                     start_ind = 0
+#                     for z in range(N+(explode_range - 1), N-1, -1):
                         
-                        grid_2[z][k] = grid_exploded[start_ind]
-                        # print("start_ind: ", start_ind)
-                        start_ind += 1
+#                         grid_2[z][k] = grid_exploded[start_ind]
+#                         # print("start_ind: ", start_ind)
+#                         start_ind += 1
 
-                except:
-                    pass
+#                 except:
+#                     pass
                 
 
 
-        # print("gravited:", grid)
-        return grid_2
+#         # print("gravited:", grid)
+#         return grid_2
 
 
-### 여기가 fucking 문제야
-def calculate(grid_3):
+# ### 여기가 fucking 문제야
+# def calculate(grid_3):
+#     count = 0
+#     for u in range(n):
+#         for y in range(n):
+#             if grid_3[u][y] >= 1:
+#                 for b in range(len(dx)):
+#                     dxs = u
+#                     dys = y
+#                     dxs += dx[b]
+#                     dys += dy[b]
+#                     if in_range(dxs,dys):
+#                         if grid_3[u][y] == grid_3[dxs][dys]:
+#                             count += 1
+#                         else:
+#                             pass
+#                     else:
+#                         pass
+#     # print(count/2)
+#     return int(count//2)
+
+
+
+
+
+# num_list = []
+
+
+
+
+# for i in range(n):
+#     for j in range(n):
+#             grid_copy = copy.deepcopy(grid)
+#             # print("original_grid: ", grid_copy)
+#             exploded_grid = explode(grid_copy,i,j)
+#             # print("exploded_grid : ", exploded_grid)
+#             gravitied_grid = gravity(exploded_grid,i,j,grid_copy[i][j])
+#             # print("gravited_grid: " , gravitied_grid)
+#             num_list.append(calculate(gravitied_grid))
+
+# print(max(num_list))
+
+import copy
+
+# 방향 설정: [남, 동, 서, 북]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+def in_range(x, y, size):
+    return 0 <= x < size and 0 <= y < size
+
+def explode(grid, i, j, size):
+    explosion_size = grid[i][j]
+    explode_set = set()
+    explode_set.add((i, j))
+    
+    if explosion_size > 1:
+        for k in range(1, explosion_size):
+            for direction in range(4):
+                ni = i + k * dx[direction]
+                nj = j + k * dy[direction]
+                if in_range(ni, nj, size):
+                    explode_set.add((ni, nj))
+    
+    for (x, y) in explode_set:
+        grid[x][y] = 0
+    
+    return grid
+
+def apply_gravity(grid, size):
+    for col in range(size):
+        new_col = []
+        # 아래에서 위로 순회하며 0이 아닌 숫자들을 수집
+        for row in range(size-1, -1, -1):
+            if grid[row][col] != 0:
+                new_col.append(grid[row][col])
+        # 수집한 숫자들을 다시 아래에서 위로 배치
+        for row in range(size-1, -1, -1):
+            if new_col:
+                grid[row][col] = new_col.pop(0)
+            else:
+                grid[row][col] = 0
+    return grid
+
+def calculate(grid_3,n):
     count = 0
     for u in range(n):
         for y in range(n):
@@ -90,7 +173,7 @@ def calculate(grid_3):
                     dys = y
                     dxs += dx[b]
                     dys += dy[b]
-                    if in_range(dxs,dys):
+                    if in_range(dxs,dys,n):
                         if grid_3[u][y] == grid_3[dxs][dys]:
                             count += 1
                         else:
@@ -100,23 +183,22 @@ def calculate(grid_3):
     # print(count/2)
     return int(count//2)
 
-
-
-
-
-num_list = []
-
-
-
-
-for i in range(n):
-    for j in range(n):
+def main():
+    n = int(input())
+    grid = [list(map(int, input().split())) for _ in range(n)]
+    
+    num_list = []
+    
+    for i in range(n):
+        for j in range(n):
             grid_copy = copy.deepcopy(grid)
-            # print("original_grid: ", grid_copy)
-            exploded_grid = explode(grid_copy,i,j)
-            # print("exploded_grid : ", exploded_grid)
-            gravitied_grid = gravity(exploded_grid,i,j,grid_copy[i][j])
-            # print("gravited_grid: " , gravitied_grid)
-            num_list.append(calculate(gravitied_grid))
+            exploded_grid = explode(grid_copy, i, j, n)
+            gravitied_grid = apply_gravity(exploded_grid, n)
+            # print("gravitied_grid: ", gravitied_grid)
+            num_pairs = calculate(gravitied_grid,n)
+            num_list.append(num_pairs)
+    
+    print(max(num_list))
 
-print(max(num_list))
+if __name__ == "__main__":
+    main()
