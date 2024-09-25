623d3b587b29d5e32c575c10ffdd318881f129e3
subiio <156166363+subiio@users.noreply.github.com>
2024-09-25 23:06:20 +0900

    Title: 아름다운 수, Time: 128ms, Memory: 28MB, Status: Passed - CodeTree

--- 240925/아름다운 수/beautiful-number.py
+++ 240925/아름다운 수/beautiful-number.py
@@ -1,55 +1,64 @@
 n = int(input())
-
+k = 4
 grid = []
 
 def print_answer():
     print(len(grid))
 answer = []
-def backtracking(curr_num):
+def backtracking(curr_num,number):
     if curr_num == n:
         answer.append(grid)
         return 
-    for i in range(1, 5):
-        if i == 1:
-            if curr_num <= n -1:
-                grid.append(i)
-                backtracking(curr_num + 1)
+    for i in range(1, number+1):
+        if curr_num <= n-i:
+            for j in range(i):
+                grid.append(j)
+            backtracking(curr_num + i,number)
+            for k in range(i):
                 grid.pop()
-            else:
-                pass
-        elif i == 2:
-            if curr_num <= n -2:
-                grid.append(i)
-                grid.append(i)
-                backtracking(curr_num + 2)
-                grid.pop()
-                grid.pop()
-            else:
-                pass
-        elif i == 3:
-            if curr_num <= n -3:
-                grid.append(i)
-                grid.append(i)
-                grid.append(i)
-                backtracking(curr_num + 3)
-                grid.pop()
-                grid.pop()
-                grid.pop()
-            else:
-                pass
-        elif i == 4:
-            if curr_num <= n -4:
-                grid.append(i)
-                grid.append(i)
-                grid.append(i)
-                grid.append(i)
-                backtracking(curr_num + 4)
-                grid.pop()
-                grid.pop()
-                grid.pop()
-                grid.pop()
-            else:
-                pass
+        else:
+            pass
 
-backtracking(0)
+        # if i == 1:
+        #     if curr_num <= n -1:
+        #         grid.append(i)
+        #         backtracking(curr_num + 1)
+        #         grid.pop()
+        #     else:
+        #         pass
+        # elif i == 2:
+        #     if curr_num <= n -2:
+        #         grid.append(i)
+        #         grid.append(i)
+        #         backtracking(curr_num + 2)
+        #         grid.pop()
+        #         grid.pop()
+        #     else:
+        #         pass
+        # elif i == 3:
+        #     if curr_num <= n -3:
+        #         grid.append(i)
+        #         grid.append(i)
+        #         grid.append(i)
+        #         backtracking(curr_num + 3)
+        #         grid.pop()
+        #         grid.pop()
+        #         grid.pop()
+        #     else:
+        #         pass
+        # elif i == 4:
+        #     if curr_num <= n -4:
+        #         grid.append(i)
+        #         grid.append(i)
+        #         grid.append(i)
+        #         grid.append(i)
+        #         backtracking(curr_num + 4)
+        #         grid.pop()
+        #         grid.pop()
+        #         grid.pop()
+        #         grid.pop()
+        #     else:
+        #         pass
+# 
+backtracking(0,k)
 print(len(answer))
