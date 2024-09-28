f3a5aee15e646a620368edadde0ec74eef0ca3a5
subiio <156166363+subiio@users.noreply.github.com>
2024-09-28 16:16:33 +0900

    Title: xor 결과 최대 만들기, Time: 169ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240928/xor 결과 최대 만들기/max-of-xor.py
+++ 240928/xor 결과 최대 만들기/max-of-xor.py
@@ -1,26 +1,30 @@
 n,m = map(int,input().split())
 # n,m = 2,1
 # n,m = 1,1
+# n,m = 20,6
+
 num_list= list(map(int,input().split()))
-# num_list = [0]
+# num_list = [13220,347018 ,441398 ,549639 ,25569,243446,716455,556546,233772,170437,770641,833442,281691,939718,111848,110910,912334,445352,127500,17219]
+#,num_list = [0]
 # num_list = [1,2]
 def xor(num1,num2):
-    cnt = 1
-    sum_number = 0
-    num_list_1 = []
-    num_list_2 = []
-    while not (num1== 0 and num2== 0):
-        num_list_1.append(num1 % 2)
-        num_list_2.append(num2 % 2)
-        num1 = num1//2 
-        num2 = num2 // 2
-    for i in range(len(num_list_1)):
-        if num_list_1[i] == num_list_2[i]:
-            sum_number += 0
-        else:
-            sum_number += 2**i
+    # sum_number = 0
+    # num_list_1 = []
+    # num_list_2 = []
+    # while not (num1== 0 and num2== 0):
+    #     num_list_1.append(num1 % 2)
+    #     num_list_2.append(num2 % 2)
+    #     num1 = num1//2 
+    #     num2 = num2//2
+    # # print("len(num_list_1)", len(num_list_1))
+    # # print("len(num_list_2)", len(num_list_2))
+    # for i in range(len(num_list_1)):
+    #     if num_list_1[i] == num_list_2[i]:
+    #         sum_number += 0
+    #     else:
+    #         sum_number += 2**i
         
-    return sum_number
+    return num1 ^ num2
 
 max_num = 0
 num_list2 = []
