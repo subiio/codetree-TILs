fe17343ba9b1fa07ce39ba7d480c9c6593c4d783
subiio <156166363+subiio@users.noreply.github.com>
2024-09-28 15:59:27 +0900

    Title: xor 결과 최대 만들기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240928/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,49 @@
+n,m = map(int,input().split())
+
+num_list= list(map(int,input().split()))
+
+def xor(num1,num2):
+    cnt = 1
+    sum_number = 0
+    num_list_1 = []
+    num_list_2 = []
+    while not (num1== 0 and num2== 0):
+        num_list_1.append(num1 % 2)
+        num_list_2.append(num2 % 2)
+        num1 = num1//2 
+        num2 = num2 // 2
+    for i in range(len(num_list_1)):
+        if num_list_1[i] == num_list_2[i]:
+            sum_number += 0
+        else:
+            sum_number += 2**i
+        
+    return sum_number
+
+max_num = 0
+num_list2 = []
+def backtracking(curnum,idx):
+    global max_num
+    if curnum == m:
+        # print(num_list2)
+        if m == 1:
+            temp = 0
+        elif m == 2:
+            temp = xor(num_list2[0],num_list2[1])
+        else:
+            first_num = num_list2[0]
+            second_num = num_list2[1]
+            temp = xor(first_num,second_num)
+            for j in range(3,m):
+                temp = xor(temp,num_list2[j])
+        max_num = max(temp,max_num)
+        return
+        
+    
+    for i in range(idx+1,n+1):
+        num_list2.append(i)
+        backtracking(curnum + 1, i)
+        num_list2.pop()    
+
+backtracking(0,0) 
+print(max_num)
