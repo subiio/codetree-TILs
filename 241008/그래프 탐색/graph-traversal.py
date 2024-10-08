55209ce748241d19163e123e175efbba7b7c2c24
subiio <156166363+subiio@users.noreply.github.com>
2024-10-08 12:33:00 +0900

    Title: 그래프 탐색, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/그래프 탐색/graph-traversal.py
@@ -0,0 +1,40 @@
+N,M = map(int,input().split())
+
+visited = [False for _ in range(N+1)]
+
+adj_list = [[False for _ in range(N+1)] for _ in range(N+1)]
+
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    adj_list[a][b] = 1
+    adj_list[b][a] = 1
+
+#  0 0 0 0 0 0 
+#  0 0 1 1 0 0
+#  0 1 0 1 0 0
+#  0 1 1 0 0 0
+#  0 0 0 0 0 1
+#  0 0 0 0 1 0
+
+VERTICES_NUM = N 
+
+
+count = 0
+def dfs(vertex):
+    global count
+    for curr_v in range(1, VERTICES_NUM + 1):
+        if adj_list[vertex][curr_v] and not visited[curr_v]:
+            count += 1
+            visited[curr_v] = True
+            dfs(curr_v)
+
+dfs(1)
+print(count - 1)
+print()
+# def dfs(vertex):
+#     for curr_v in graph[vertex]:
+#         if not visited[curr_v]:
+#             print(curr_v)
+#             visited[curr_v] = True
+#             dfs(curr_v)
