f77172b0759376ffe976f177fac5f05190d7c76e
subiio <156166363+subiio@users.noreply.github.com>
2024-10-08 13:55:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241008/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,42 @@
+n,m = map(int,input().split())
+
+grid = []
+
+for _ in range(n):
+    grid.append(list(map(int,input().split())))
+
+
+visited = [[False for _ in range(m)] for _ in range(n)]
+
+def in_range(i,j):
+    global n,m 
+    return (0 <= i < m) and (0 <= j < n)
+
+
+def cango(i,j):
+    if not in_range(i,j):
+        return False
+    elif visited[i][j] == True or grid[i][j] == 0:
+        return False
+
+    return True
+
+count = 0
+def dfs(x,y):
+
+    global grid
+
+    dxs , dys = [1,0], [0, 1]
+
+    for dx, dy in zip(dxs, dys):
+        new_x , new_y = x + dx , y + dy
+        
+        if cango(new_x,new_y):
+            # print("x,y: ", new_x, new_y)
+            if new_x == m-1 and new_y == n-1:
+                count += 1
+            visited[new_x][new_y] = True
+            dfs(new_x,new_y)
+
+dfs(0,0)
+print(count)
