faac0b4f6f0977cb8cd73e6ac2db1625d0ac3467
subiio <156166363+subiio@users.noreply.github.com>
2024-10-11 12:57:51 +0900

    Title: 외판원 순회, Time: 204ms, Memory: 26MB, Status: Passed - CodeTree

--- 241011/외판원 순회/traveling-salesman-problem.py
+++ 241011/외판원 순회/traveling-salesman-problem.py
@@ -1,39 +1,27 @@
 n = int(input())
-grid= []
+grid = []
 for _ in range(n):
-    grid.append(list(map(int,input().split())))
+    grid.append(list(map(int, input().split())))
 
-visited = [False for _ in range(n+1)]
+visited = [False for _ in range(n)]  # n 크기의 방문 여부 리스트
+min_cost = float('inf')
 
-answer= [1]
-
-min_cost = 1000000
-def backtracking(curr_num):
+def backtracking(curr_num, current_cost, start_node, path_len):
     global min_cost
-    if curr_num == n-1:
-        sum_number = 0
-       
-        for k in range(0,n):
-            if k == n-1:
-                sum_number += grid[answer[k]-1][0]
-            else:
-                # print(answer)
-                sum_number += grid[answer[k]-1][answer[k+1]-1]
-        min_cost = min(sum_number, min_cost)
 
+    # 모든 도시를 방문한 후 다시 시작점으로 돌아옴
+    if path_len == n and grid[curr_num][start_node] > 0:
+        min_cost = min(min_cost, current_cost + grid[curr_num][start_node])
         return
 
-    for i in range(2,n+1):
-        if visited[i] == True:
-            continue
-        
-        visited[i] = True
-        answer.append(i)
-        backtracking(curr_num+1)
-        answer.pop()
-        visited[i] = False
+    for next_node in range(n):
+        if not visited[next_node] and grid[curr_num][next_node] > 0:
+            visited[next_node] = True
+            backtracking(next_node, current_cost + grid[curr_num][next_node], start_node, path_len + 1)
+            visited[next_node] = False
 
+# 1번 노드를 시작점으로 설정
+visited[0] = True
+backtracking(0, 0, 0, 1)
 
-backtracking(0)
-        
 print(min_cost)
