82ee9a309d087053138078d9fea83801cf049534
subiio <156166363+subiio@users.noreply.github.com>
2024-10-11 10:36:39 +0900

    Title: 돌 잘 치우기, Time: 441ms, Memory: 30MB, Status: Passed - CodeTree

--- 241011/돌 잘 치우기/clear-stones-well.py
+++ 241011/돌 잘 치우기/clear-stones-well.py
@@ -1,7 +1,5 @@
 from collections import deque
 import copy
-
-
 n,k,m = map(int,input().split())
 grid = []
 for _ in range(n):
@@ -17,12 +15,11 @@ q = deque()
 def in_range(x,y):
     return (0<= x < n) and (0 <= y < n)
 
-
-
 def can_go(x,y):
     if not in_range(x,y):
         return False
-    if visited[x][y] == True or pyeong_list[x][y] == 1:
+
+    elif pyeongga_list[x][y] == 1 or visited[x][y] == True:
         return False
     return True
 
@@ -30,10 +27,8 @@ def push(x,y):
     visited[x][y] = True
     q.append((x,y))
 
-
 def bfs():
-
-    dxs, dys = [1,0,-1,0],[0,1,0,-1]
+    dxs, dys = [1,0,-1,0], [0,1,0,-1]
 
     while q:
         curr_x, curr_y = q.popleft()
@@ -41,53 +36,68 @@ def bfs():
             new_x, new_y = curr_x + dx, curr_y + dy
             if can_go(new_x,new_y):
                 push(new_x,new_y)
-
-def find_ind(grid):
-    result = []
+def find_ind_1(grid):
+    ind_list = []
     for i in range(n):
         for j in range(n):
             if grid[i][j] == 1:
-                result.append([i,j])
-    return result
+                ind_list.append([i,j])
+    return ind_list
 
-def find_sublist(list,idx,current,result):
-    if idx == m:
+index_list = find_ind_1(grid)
 
-        result.append(current[:])
 
-    
-    for i in range(idx, len(list)):
-        current.append(list[i])
-        find_sublist(list,i+1,current,result)
+# def find_list(ind_list,m):
+#     start_ind = 0
+#     list_holy = []
+#     while start_ind != (len(ind_list) - m + 1):
+#         start_ind += 1
+def backtracking(ind_list,idx,current,result):
+
+    result.append(current[:])
+
+    for i in range(idx, len(ind_list)):
+        current.append(ind_list[i])
+
+        backtracking(ind_list, i + 1, current, result)
+
         current.pop()
-    
-    
 
-    
+def get_all_sublists(ind_list):
+    result = []
+    backtracking(ind_list,0, [], result)
+    return result
+        
 
-list_list = find_ind(grid)
+sublist = get_all_sublists(index_list)
+gwansim_list = []
+for i in sublist:
+    if len(i) == m:
+        gwansim_list.append(i)
+# print(gwansim_list)
 
-result = []
-sublist = find_sublist(list_list,0,[],result)
 max_number = 0
 count = 0
-for j in result:
-
-    pyeong_list = copy.deepcopy(grid)
+for j in gwansim_list:
+    # count = 0
+    pyeongga_list = copy.deepcopy(grid)
     visited = [[False for _ in range(n)] for _ in range(n)]
     for k in range(len(j)):
-        ind_x, ind_y = j[k][0], j[k][1]
-        pyeong_list[ind_x][ind_y] = 0
+        ind_x, ind_y  = j[k][0], j[k][1]
+        pyeongga_list[ind_x][ind_y] = 0
+        # count = 0
     for i in start_point:
-        x,y = i[0]-1, i[1] -1
+        x,y = i[0]-1, i[1]-1 
         push(x,y)
         bfs()
-
     for t in range(n):
         for r in range(n):
             if visited[t][r] == True:
                 count += 1
+
+
     max_number = max(max_number,count)
     count = 0
+            
 
 print(max_number)
