f33b0c6adc5a6f4145da609e004d2e94efdbfc98
subiio <156166363+subiio@users.noreply.github.com>
2024-10-15 23:25:59 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241015/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double feet = 9.2, mile = 1.3;
+
+    printf("%.1lfft = %.1lfcm\n%.1lfmi = %.1lfcm",feet,feet*30.48,mile,mile*160934);
+    return 0;
+}
