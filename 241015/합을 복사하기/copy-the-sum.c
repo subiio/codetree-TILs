235bcc9f986938f3ab9b452454bbe7b4bdb0983f
subiio <156166363+subiio@users.noreply.github.com>
2024-10-15 23:41:17 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241015/합을 복사하기/copy-the-sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a =1, b=2 ,c=3;
+    int summation;
+    summation = a+b+c;
+    a=b=c=summation;
+    printf("%d %d %d",a,b,c);
+    return 0;
+}
